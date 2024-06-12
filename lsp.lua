9a8a027544c395856f10e87a9f74f8fe7be1e4f6
ReFantasy <309741309@qq.com>
2024-06-12 22:42:22 +0800

    .

--- lsp.lua
+++ lsp.lua
@@ -13,22 +13,6 @@ return {
           grr = false,
         },
       },
-      config = {
-        clangd = {
-          cmd = {
-            "clangd",
-            "--background-index",
-            "--clang-tidy",
-            "--all-scopes-completion",
-            "--completion-style=detailed",
-            "--j=8",
-            "--pch-storage=memory",
-          },
-          capabilities = {
-            offsetEncoding = "utf-8",
-          },
-        },
-      },
     },
   },
 
@@ -44,7 +28,7 @@ return {
 
           -- 定义新的映射
           K = {
-            function() vim.lsp.buf.hover() end,
+            function() vim.api.nvim_command "Lspsaga hover_doc" end,
             desc = "Hover symbol details",
           },
           ["<Leader>fm"] = {
@@ -52,11 +36,16 @@ return {
             desc = "[C] Format code",
           },
           ["<Leader>ca"] = {
-            function() vim.lsp.buf.code_action() end,
+            function() vim.api.nvim_command "Lspsaga code_action" end,
             desc = "[C] Code action",
           },
+          ["<Leader>da"] = {
+            function() vim.api.nvim_command "Lspsaga show_buf_diagnostics" end,
+            desc = "[C] Code action",
+          },
+
           ["<Leader>rn"] = {
-            function() vim.lsp.buf.rename() end,
+            function() vim.api.nvim_command "Lspsaga rename" end,
             desc = "[C] Rename",
           },
           gr = {
@@ -73,13 +62,15 @@ return {
     ---@param opts AstroLSPOpts
     opts = function(_, opts)
       opts.mappings.n.gd[1] = function() vim.api.nvim_command "Lspsaga goto_definition" end
-      opts.mappings.n.gI[1] = function() require("telescope.builtin").lsp_implementations { reuse_win = true } end
-      opts.mappings.n.gr[1] = function() require("telescope.builtin").lsp_references() end
+      -- opts.mappings.n.gI[1] = function() require("telescope.builtin").lsp_implementations { reuse_win = true } end
+
+      -- opts.mappings.n.gr[1] = function() require("telescope.builtin").lsp_references() end
+      opts.mappings.n.gr[1] = function() vim.api.nvim_command "Lspsaga finder" end
+
       opts.mappings.n.gy[1] = function() require("telescope.builtin").lsp_type_definitions { reuse_win = true } end
       opts.mappings.n["<Leader>lG"][1] = function()
         vim.ui.input({ prompt = "Symbol Query: (leave empty for word under cursor)" }, function(query)
           if query then
-            -- word under cursor if given query is empty
             if query == "" then query = vim.fn.expand "<cword>" end
             require("telescope.builtin").lsp_workspace_symbols {
               query = query,
